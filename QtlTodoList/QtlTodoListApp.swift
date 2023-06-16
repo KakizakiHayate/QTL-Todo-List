9450df44ae9d6918dcbdf823eec295de34626104
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-17 00:51:29 +0900

    プロジェクト作成

---
+++ QtlTodoList/QtlTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  QtlTodoListApp.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct QtlTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
