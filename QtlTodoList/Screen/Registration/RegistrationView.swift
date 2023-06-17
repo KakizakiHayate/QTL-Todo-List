ee126d5108f4c67a1fffe4e49917da3eb6d395ea
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-17 18:36:57 +0900

    新規登録画面を作成

---
+++ QtlTodoList/Screen/Registration/RegistrationView.swift
@@ -0,0 +1,37 @@
+//
+//  RegistrationView.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/06/17.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @StateObject private var registrationViewModel = RegistrationViewModel()
+    var body: some View {
+        VStack {
+            TextField("名前を入力", text: $registrationViewModel.name)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("メールアドレスを入力", text: $registrationViewModel.email)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("パスワードを入力", text: $registrationViewModel.password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            Button {
+                registrationViewModel.registration()
+                print("sss")
+            } label: {
+                Text(AppConst.Text.registration)
+            }
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegistrationView()
+    }
+}
