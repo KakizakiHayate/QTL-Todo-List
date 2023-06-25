10b1ab5ead74eae1a861eba88fdf1cc2f2c3f418
kakizaki hayate <hayate.h.0704@gmail.com>
2023-06-25 12:28:32 +0900

    UpdateTodoViewファイルをManagerクラスの作成により修正とファイルの移動

---
+++ QtlTodoList/Screen/TodoDetails/UpdateTodo/UpdateTodoView.swift
@@ -0,0 +1,42 @@
+//
+//  UpdateTodoView.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/06/17.
+//
+
+import SwiftUI
+
+struct UpdateTodoView: View {
+    // MARK: - Property Wrappers
+    @StateObject private var firebaseManager  = FirebaseManager.shared
+    @Environment(\.dismiss) private var dismiss
+
+    // MARK: - body
+    var body: some View {
+        ScrollView {
+            VStack {
+                TextField("入力", text: $firebaseManager.todo.title)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextEditor(text: $firebaseManager.todo.message)
+                    .frame(height: 250)
+                    .border(.gray, width: 2)
+                    .padding()
+                Button {
+                    firebaseManager.updateFirestoreData(todo: firebaseManager.todo)
+                    dismiss()
+                } label: {
+                    Text("完了")
+                }.padding()
+            }
+        }
+    } // body
+} // view
+
+// MARK: - Preview
+struct UpdateTodoView_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateTodoView()
+    }
+}
