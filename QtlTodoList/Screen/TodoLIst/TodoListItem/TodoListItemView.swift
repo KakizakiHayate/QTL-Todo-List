62a0658b626eacd93081e248f82c8de5f595e223
kakizaki hayate <hayate.h.0704@gmail.com>
2023-07-17 23:40:40 +0900

    TodoListの更新と削除をTodoListDetailViewで行うように修正

--- QtlTodoList/Screen/TodoLIst/TodoListItem/TodoListItemView.swift
+++ QtlTodoList/Screen/TodoLIst/TodoListItem/TodoListItemView.swift
@@ -15,41 +15,24 @@ struct TodoListItemView: View {
 
     // MARK: - body
     var body: some View {
-        ForEach(todos) { todo in
-            VStack(alignment: .leading) {
-                HStack {
-                    Text(todo.title)
-                    Spacer()
-                    Image(systemName: "pencil")
-                        .foregroundColor(.customColorEmeraldGreen)
-                        .font(.title)
-                        .onTapGesture {
-                            firebaseManager.todo = todo
-                            isTodoUpdateDetails.toggle()
-                        }.navigationDestination(isPresented: $isTodoUpdateDetails) {
-                            UpdateTodoView()
-                        }.padding(.trailing)
-                    Image(systemName: "trash")
-                        .foregroundColor(.customColorEmeraldGreen)
-                        .font(.title2)
-                        .onTapGesture {
-                            Task {
-                                await firebaseManager.deleteFirestoreData(todo: todo)
-                                guard let todos = await firebaseManager.readFirestoreData() else {
-                                    return
-                                }
-                                self.todos = todos
-                            }
-                        }
-                }.padding(.bottom)
-                Text(todo.message)
+        ForEach($todos) { todo in
+                NavigationLink {
+                    TodoListDetailView(todos: todo.projectedValue)
+                } label: {
+                VStack(alignment: .leading) {
+                        HStack {
+                            Text(todo.wrappedValue.title)
+                        }.padding(.bottom)
+                            Text(todo.wrappedValue.message)
+                        .font(.caption2)
+                    }
+                }
             }
-        }
     } // body
 } // view
 
 // MARK: - Preview
-struct TodoListDetailView_Previews: PreviewProvider {
+struct TodoListItemView_Previews: PreviewProvider {
     static var previews: some View {
         TodoListItemView(todos: .constant([]))
     }
