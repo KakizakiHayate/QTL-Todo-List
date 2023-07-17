e9578c7df2ed9a192ad14576a40001ae6d28353f
kakizaki hayate <hayate.h.0704@gmail.com>
2023-07-18 02:53:04 +0900

    ScrollViewを追加

--- QtlTodoList/Screen/TodoLIst/TodoListItem/TodoListDetail/TodoListDetailView.swift
+++ QtlTodoList/Screen/TodoLIst/TodoListItem/TodoListDetail/TodoListDetailView.swift
@@ -17,29 +17,31 @@ struct TodoListDetailView: View {
     // MARK: - body
     var body: some View {
         VStack {
-            GeometryReader { proxy in
-                let frameWidth = proxy.size.width / 1.3
-                Text(todos.title)
-                    .font(.callout)
-                    .padding()
-                    .frame(width: frameWidth,
-                           alignment: .leading)
-                    .background(.white)
-                    .cornerRadius(8)
-                    .shadow(color: .gray.opacity(0.7), radius: 5)
-                    .offset(x: (proxy.size.width - (frameWidth)) / equalSpacing,
-                            y: UIScreen.main.bounds.height * 0.03)
-                Text(todos.message)
-                    .font(.caption)
-                    .padding()
-                    .frame(width: frameWidth,
-                           height: UIScreen.main.bounds.height * 0.5,
-                           alignment: .topLeading)
-                    .background(.white)
-                    .cornerRadius(8)
-                    .shadow(color: .gray.opacity(0.7), radius: 5)
-                    .offset(x: (proxy.size.width - (frameWidth)) / equalSpacing,
-                            y: UIScreen.main.bounds.height * 0.2)
+            ScrollView {
+                GeometryReader { proxy in
+                    let frameWidth = proxy.size.width / 1.3
+                    Text(todos.title)
+                        .font(.callout)
+                        .padding()
+                        .frame(width: frameWidth,
+                               alignment: .leading)
+                        .background(.white)
+                        .cornerRadius(8)
+                        .shadow(color: .gray.opacity(0.7), radius: 5)
+                        .offset(x: (proxy.size.width - (frameWidth)) / equalSpacing,
+                                y: UIScreen.main.bounds.height * 0.03)
+                    Text(todos.message)
+                        .font(.caption)
+                        .padding()
+                        .frame(width: frameWidth,
+                               height: UIScreen.main.bounds.height * 0.5,
+                               alignment: .topLeading)
+                        .background(.white)
+                        .cornerRadius(8)
+                        .shadow(color: .gray.opacity(0.7), radius: 5)
+                        .offset(x: (proxy.size.width - (frameWidth)) / equalSpacing,
+                                y: UIScreen.main.bounds.height * 0.2)
+                }
             }
             UpdateDeleteButtonView(todos: $todos)
         }
