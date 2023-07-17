52a6e18f50a811a54aec461cbd11646ed3701be4
kakizaki hayate <hayate.h.0704@gmail.com>
2023-07-17 23:50:38 +0900

    todoを更新と削除を選択するViewを追加

---
+++ QtlTodoList/Screen/TodoLIst/TodoListItem/TodoListDetail/TodoListDetailView.swift
@@ -0,0 +1,89 @@
+//
+//  TodoListDetailView.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/07/14.
+//
+
+import SwiftUI
+
+struct TodoListDetailView: View {
+    // MARK: - Property Wrappers
+    @State private var isUpdateTodoView = false
+    @State private var isDeleteAlert = false
+    @Binding var todos: Todos
+    @StateObject private var firebaseManager = FirebaseManager.shared
+    // MARK: - Properties
+    private let equalSpacing: CGFloat = 2
+
+    // MARK: - body
+    var body: some View {
+        VStack {
+            GeometryReader { proxy in
+                let frameWidth = proxy.size.width / 1.3
+                Text(todos.title)
+                    .font(.callout)
+                    .padding()
+                    .frame(width: frameWidth, alignment: .leading)
+                    .background(.white)
+                    .cornerRadius(8)
+                    .shadow(color: .gray.opacity(0.7), radius: 5)
+                    .offset(x: (proxy.size.width - (frameWidth)) / equalSpacing,
+                            y: UIScreen.main.bounds.height * 0.03)
+                Text(todos.message)
+                    .font(.caption)
+                    .padding()
+                    .frame(width: frameWidth, height: UIScreen.main.bounds.height * 0.5, alignment: .topLeading)
+                    .background(.white)
+                    .cornerRadius(8)
+                    .shadow(color: .gray.opacity(0.7), radius: 5)
+                    .offset(x: (proxy.size.width - (frameWidth)) / equalSpacing,
+                            y: UIScreen.main.bounds.height * 0.2)
+            }
+            HStack {
+                Button {
+                    isUpdateTodoView.toggle()
+                } label: {
+                    Image(systemName: "pencil")
+                }.foregroundColor(.white)
+                    .padding()
+                    .font(.system(size: 30))
+                    .background(
+                        RoundedRectangle(cornerRadius: 40)
+                            .fill(Color.customColorEmeraldGreen)
+                    ).padding()
+                    .navigationDestination(isPresented: $isUpdateTodoView) {
+                        UpdateTodoView(todos: $todos)
+                    }
+                Button {
+                    isDeleteAlert.toggle()
+                } label: {
+                    Image(systemName: "trash")
+                }.foregroundColor(.white)
+                    .padding()
+                    .font(.system(size: 25))
+                    .background(
+                        RoundedRectangle(cornerRadius: 40)
+                            .fill(Color.customColorEmeraldGreen)
+                    ).padding()
+                    .alert("このタスクを削除しますか？", isPresented: $isDeleteAlert) {
+                        Button {} label: { Text("キャンセル") }
+                        Button {
+                            Task {
+                                firebaseManager.deleteFirestoreData(todo:)
+                            }
+                        } label: {
+                            Text("OK")
+                        }
+                    }
+            }
+        }
+    } // body
+} // view
+
+// MARK: - Preview
+struct TodoListDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodoListDetailView(todos: .constant(Todos(title: "ああああ￥", message: "testt")))
+    }
+}
