b5efcfd158146ca59542c40bb81c805fa69ae193
kakizaki hayate <hayate.h.0704@gmail.com>
2023-07-17 23:43:19 +0900

    BindingPropertyWrappersを追加

--- QtlTodoList/Screen/TodoDetails/UpdateTodo/UpdateTodoView.swift
+++ QtlTodoList/Screen/TodoDetails/UpdateTodo/UpdateTodoView.swift
@@ -11,21 +11,24 @@ struct UpdateTodoView: View {
     // MARK: - Property Wrappers
     @StateObject private var firebaseManager  = FirebaseManager.shared
     @Environment(\.dismiss) private var dismiss
+    @Binding var todos: Todos
 
     // MARK: - body
     var body: some View {
         ScrollView {
             VStack {
-                TextField("入力", text: $firebaseManager.todo.title)
+                TextField("入力", text: $todos.title)
                     .padding()
                     .textFieldStyle(RoundedBorderTextFieldStyle())
-                TextEditor(text: $firebaseManager.todo.message)
+                TextEditor(text: $todos.message)
                     .frame(height: 250)
                     .border(.gray, width: 2)
                     .padding()
                 Button {
-                    firebaseManager.updateFirestoreData(todo: firebaseManager.todo)
-                    dismiss()
+                    Task {
+                        await firebaseManager.updateFirestoreData(todo: todos)
+                        dismiss()
+                    }
                 } label: {
                     Text("完了")
                 }.padding()
@@ -37,6 +40,6 @@ struct UpdateTodoView: View {
 // MARK: - Preview
 struct UpdateTodoView_Previews: PreviewProvider {
     static var previews: some View {
-        UpdateTodoView()
+        UpdateTodoView(todos: .constant(Todos(title: "", message: "")))
     }
 }
