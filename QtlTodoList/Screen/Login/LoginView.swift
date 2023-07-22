c435afae97a728e42fba9c14cba42ff98fd71b46
kakizaki hayate <hayate.h.0704@gmail.com>
2023-07-22 18:52:51 +0900

    ログイン画面のボタンのデザインを変更

--- QtlTodoList/Screen/Login/LoginView.swift
+++ QtlTodoList/Screen/Login/LoginView.swift
@@ -11,37 +11,66 @@ struct LoginView: View {
     // MARK: - Property Wrappers
     @StateObject private var loginViewModel = LoginViewModel()
     @State private var isRegistrationView = false
+    // MARK: Properties
+    private let screenSize = UIScreen.main.bounds.height
     // MARK: - body
     var body: some View {
-        NavigationStack {
-            VStack {
-                TextField("メールアドレスを入力", text: $loginViewModel.email)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                TextField("パスワードを入力", text: $loginViewModel.password)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .padding()
-                Button {
-                    loginViewModel.login()
-                } label: {
-                    Text("ログインする")
-                }.padding()
-                Button {
-                    loginViewModel.googleLogin()
-                } label: {
-                    Text("Googleログイン")
-                }.padding()
-                Button {
-                    isRegistrationView.toggle()
-                } label: {
-                    Text("新規登録はコチラ")
-                }.padding()
-            }
-            .navigationDestination(isPresented: $loginViewModel.isTodoView) {
-                TodoView()
-            }
-            .navigationDestination(isPresented: $isRegistrationView) {
-                RegistrationView()
+        GeometryReader { proxy in
+            let width = proxy.size.width
+            let height = proxy.size.height
+            NavigationStack {
+                VStack {
+                    Spacer()
+                    TextField("メールアドレスを入力", text: $loginViewModel.email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    TextField("パスワードを入力", text: $loginViewModel.password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                    Button {
+                        loginViewModel.login()
+                    } label: {
+                        Text("ログインする")
+                    }.padding()
+                        .background(Color.customColorEmeraldGreen)
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .cornerRadius(30)
+                    Spacer().frame(height: height / 15)
+                    Text("--- または ---").foregroundColor(.gray)
+                    Button {
+                        loginViewModel.googleLogin()
+                    } label: {
+                        Text("Googleログイン")
+                    }.padding()
+                        .font(.headline)
+                        .foregroundColor(.customColorEmeraldGreen)
+                        .background(.white)
+                        .cornerRadius(30)
+                        .compositingGroup()
+                        .shadow(color: .gray.opacity(0.7) ,radius: 3)
+                        .padding()
+                    Text("--- 登録されてない方はコチラ ---")
+                        .foregroundColor(.gray)
+                    Button {
+                        isRegistrationView.toggle()
+                    } label: {
+                        Text("新規登録")
+                    }.padding()
+                        .font(.headline)
+                        .foregroundColor(.customColorEmeraldGreen)
+                        .background(.white)
+                        .cornerRadius(30)
+                        .compositingGroup()
+                        .shadow(color: .gray.opacity(0.7) ,radius: 3)
+                        .padding()
+                }
+                .navigationDestination(isPresented: $loginViewModel.isTodoView) {
+                    TodoView()
+                }
+                .navigationDestination(isPresented: $isRegistrationView) {
+                    RegistrationView()
+                }
             }
         }
     } // body
