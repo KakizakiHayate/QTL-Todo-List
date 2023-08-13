ddbb65f33b89070b135045b60931bb981cf2d99c
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-13 18:00:30 +0900

    LoginViewModelで具体的なエラーを取得できるようにした

--- QtlTodoList/Screen/Login/LoginViewModel.swift
+++ QtlTodoList/Screen/Login/LoginViewModel.swift
@@ -15,24 +15,39 @@ class LoginViewModel: ObservableObject {
     @Published var email = "test@example.com"
     @Published var password = "password"
     @Published var isTodoView = false
-    @Published var registrationFailureAlert = false
+    @Published var loginFailureAlert = false
+    @Published var googleLoginFailureAlert = false
+    @Published var errorMessage = FirebaseAuthError.unknown.title
 }
 
 extension LoginViewModel {
     // MARK: - Methods
     /// メールアドレス/パスワードでログイン
-    func login() {
-        Task {
-            do {
-                try await Auth.auth().signIn(withEmail: email, password: password)
-                Task { @MainActor in
-                    isTodoView.toggle()
+    func login() async -> Bool {
+        do {
+            try await Auth.auth().signIn(withEmail: email, password: password)
+            print("ログインしました")
+            return true
+        } catch {
+            guard let error = error as NSError? else { return false }
+            let errorCode = AuthErrorCode.Code(rawValue: error.code)
+            Task { @MainActor in
+                switch errorCode {
+                case .invalidEmail:
+                    errorMessage = FirebaseAuthError.invalidEmail.title
+                case .emailAlreadyInUse:
+                    errorMessage = FirebaseAuthError.emailAlreadyInUse.title
+                case .userNotFound:
+                    errorMessage = FirebaseAuthError.userNotFound.title
+                case .networkError:
+                    errorMessage = FirebaseAuthError.networkError.title
+                case .wrongPassword:
+                    errorMessage = FirebaseAuthError.wrongPassword.title
+                default:
+                    errorMessage = FirebaseAuthError.unknown.title
                 }
-                print("ログインしました")
-            } catch let error {
-                registrationFailureAlert.toggle()
-                print(error.localizedDescription)
             }
+            return false
         }
     }
 
@@ -42,23 +57,24 @@ extension LoginViewModel {
         let config = GIDConfiguration(clientID: clientID)
 
         let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-        let rootViewController = windowScene?.windows.first!.rootViewController!
+        guard let rootViewController = windowScene?.windows.first?.rootViewController else {
+            return
+        }
 
         GIDSignIn.sharedInstance.configuration = config
 
-        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController!) { result, error in
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { result, error in
             guard error == nil else {
-                print("GIDSignInError: \(error!.localizedDescription)")
+                print("GIDSignInError: \(error?.localizedDescription ?? "error nil::::")")
                 return
             }
-            guard let user = result?.user,
-                  let idToken = user.idToken?.tokenString else {
-                return
-            }
+            guard let user = result?.user else { return }
+            guard let idToken = user.idToken?.tokenString else { return }
             GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
-            Task.detached { @MainActor in
+            Task { @MainActor in
                 self.isTodoView.toggle()
             }
         }
+        return
     }
 }
