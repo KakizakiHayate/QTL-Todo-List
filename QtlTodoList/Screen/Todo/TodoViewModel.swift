efd7d8169e0fc95d233487ffa33da2cdf45192d1
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-13 18:08:57 +0900

    TodoViewModelでサインアウト処理を追加

--- QtlTodoList/Screen/Todo/TodoViewModel.swift
+++ QtlTodoList/Screen/Todo/TodoViewModel.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import FirebaseAuth
 
 class TodoViewModel: ObservableObject {
     // MARK: - Property Wrappers
     @Published var isTodoAddDetails = false
+    @Published var isConfirmationSignOut = false
+    @Published var isSignOutFailureAlert = false
+}
+
+extension TodoViewModel {
+    func signOut() async -> Bool {
+        do {
+            try Auth.auth().signOut()
+            print("サインアウトしました")
+            return true
+        } catch {
+            print("サインアウトに失敗しました")
+            return false
+        }
+    }
 }
