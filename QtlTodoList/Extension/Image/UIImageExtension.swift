159e2258ffd8712e0bdade5b252a1edd11d21d9e
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-22 00:15:29 +0900

    UIImageをリサイズする関数をextensionした

---
+++ QtlTodoList/Extension/Image/UIImageExtension.swift
@@ -0,0 +1,27 @@
+//
+//  UIImageExtension.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/08/19.
+//
+
+import SwiftUI
+
+extension UIImage {
+    // MARK: Methods
+    func resizeUIImage(width: CGFloat, height: CGFloat) -> UIImage? {
+        // 指定された画像の大きさのコンテキストを用意.
+        UIGraphicsBeginImageContext(CGSize(width: width, height: height))
+
+        // コンテキストに自身に設定された画像を描画する.
+        self.draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        // コンテキストからUIImageを作る.
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+
+        // コンテキストを閉じる.
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+}
