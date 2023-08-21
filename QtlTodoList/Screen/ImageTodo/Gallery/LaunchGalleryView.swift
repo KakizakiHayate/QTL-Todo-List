3e3d7fda42a266913ecd9c2ef116e9f964ba6bfa
kakizaki hayate <hayate.h.0704@gmail.com>
2023-08-22 00:17:12 +0900

    写真をギャラリーから選択できるようにした

---
+++ QtlTodoList/Screen/ImageTodo/Gallery/LaunchGalleryView.swift
@@ -0,0 +1,56 @@
+//
+//  LaunchGalleryView.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/08/19.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct LaunchGalleryView: UIViewControllerRepresentable {
+    // MARK: Property Wrappers
+    @Binding var image: UIImage
+    @Binding var isLaunchGalleryView: Bool
+
+    class Coordinator: NSObject, UINavigationControllerDelegate, PHPickerViewControllerDelegate {
+        let parent: LaunchGalleryView
+        init(parent: LaunchGalleryView) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            self.parent.isLaunchGalleryView = false
+
+            if let itemProvider = results.first?.itemProvider, itemProvider.canLoadObject(ofClass: UIImage.self) {
+                itemProvider.loadObject(ofClass: UIImage.self) { [weak self] image, _ in
+                    guard let image = image as? UIImage else {
+                        return
+                    }
+                    Task { @MainActor in
+                        self?.parent.image = image
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension LaunchGalleryView {
+    // MARK: Methods
+    func makeCoordinator() -> Coordinator { Coordinator(parent: self) }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<LaunchGalleryView>) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+
+        configuration.filter = .images
+        configuration.selectionLimit = 1
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController,
+                                context: UIViewControllerRepresentableContext<LaunchGalleryView>) {}
+}
+
