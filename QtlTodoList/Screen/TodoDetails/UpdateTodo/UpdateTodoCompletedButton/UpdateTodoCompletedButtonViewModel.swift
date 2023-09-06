823dc9e11f194bbdc7f8d8077709e39856cf06ba
kakizaki hayate <hayate.h.0704@gmail.com>
2023-09-06 18:54:51 +0900

    更新完了ボタンのViewModelを追加

---
+++ QtlTodoList/Screen/TodoDetails/UpdateTodo/UpdateTodoCompletedButton/UpdateTodoCompletedButtonViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  UpdateTodoCompletedButtonViewModel.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/09/06.
+//
+
+import Foundation
+import SwiftUI
+
+class UpdateTodoCompletedButtonViewModel: ObservableObject {
+    // MARK: - Property Wrappers
+    @ObservedObject private var firebaseManager = FirebaseManager.shared
+}
+
+extension UpdateTodoCompletedButtonViewModel {
+    // MARK: - Methods
+    func imageUploadAndUpdateTodo(todoImage: UIImage,
+                                  todos: Todos
+    ) async {
+        let uploadUrl = await firebaseManager.todoImageUpload(image: todoImage)
+        await firebaseManager.updateFirestoreData(todo: todos, uploadUrl: uploadUrl)
+    }
+}
