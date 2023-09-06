cb54a66c68ed73d68a9b5661cc5bfe8d09d71c4a
kakizaki hayate <hayate.h.0704@gmail.com>
2023-09-07 01:30:25 +0900

    通知許可orキャンセルできるようにした

---
+++ QtlTodoList/AppDelegate/AppDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AppDelegate.swift
+//  QtlTodoList
+//
+//  Created by 柿崎逸 on 2023/09/06.
+//
+
+import Foundation
+import UIKit
+
+class AppDelegate: NSObject {}
+
+extension AppDelegate: UIApplicationDelegate {
+    // MARK: - Methods
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        // 通知許可の取得
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: .alert) {
+            guard $1 != nil else { return }
+            $0 ? print("通知許可") : print("通知キャンセル")
+        }
+        return true
+    }
+}
+
+//  アプリ起動中に通知を受け取ったとき
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    // MARK: - Methods
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .banner])
+    }
+}
