78c4316c4cb092bcf53adfbf12e67fcc85919fc2
kakizaki hayate <hayate.h.0704@gmail.com>
2023-09-08 22:59:16 +0900

    追加時にローカルプッシュ通知ができるメソッド追加した

--- QtlTodoList/Screen/TodoDetails/AddTodo/AddTodoCompletedButton/AddTodoCompletedButtonViewModel.swift
+++ QtlTodoList/Screen/TodoDetails/AddTodo/AddTodoCompletedButton/AddTodoCompletedButtonViewModel.swift
@@ -27,4 +27,30 @@ extension AddTodoCompletedButtonViewModel {
         title.wrappedValue = ""
         message.wrappedValue = ""
     }
+
+    func sendNotificationRequest(notificationDate: Date,
+                                 isNotification: Bool,
+                                 title: String,
+                                 message: String
+    ) async {
+        guard isNotification else { return }
+        let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: notificationDate)
+        print(triggerDate)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = message
+        content.sound = UNNotificationSound.defaultRingtone
+
+        let request = UNNotificationRequest.init(
+            identifier: "CalendorNotification",
+            content: content,
+            trigger: trigger)
+        let center = UNUserNotificationCenter.current()
+        do {
+            try await center.add(request)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
 }
